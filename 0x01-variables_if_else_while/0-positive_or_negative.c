9d93862c482a7f57d561ccd300e6d3070b0f4bcf
SDEVFolakemi <olawumioderanti@gmail.com>
2023-02-23 15:00:36 -0800

    +/-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
-1  #include <stdlib.h>
-2  #include <time.h>
-3  #include <stdio.h>
+1 #include <stdlib.h>
+2 #include <time.h>
+3 #include <stdio.h>
 4
-5 /*
+5 /**
 6 *main -print random numbers to n whenever it is executed
 7 *
 8*Return: 0
@@ -17,7 +17,7 @@
 17
 18  if (n > 0)
 19  {
-20  printf("%d is positive\n", n);
+20  printf("%d is negative\n", n);
 21  }
 22  else if (n == 0)
 23  {
@@ -25,7 +25,7 @@
 25 }
 26  else
 27   {
-28  printf("%d is negative\n", n);
+28  printf("%d is positive\n", n);
 29  }
 30  return (0);
 31  }
