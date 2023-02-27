f81780c08b1d2cb7276e91e5204e9f7bf9daaaab
SDEVFolakemi <olawumioderanti@gmail.com>
2023-02-27 15:45:48 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,8 +3,9 @@
  * _strlen - return the length of a string
  * Description: this code returns the length of a string
  * Return: 0 is success
+ * @s: to check
  */
-int _strlen(char *s)
+int _strlen(char *s);
 {
 	int a = 0;
 
