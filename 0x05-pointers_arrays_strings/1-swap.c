4527bb2426beaa5aad78d38bff570018fe1943f6
SDEVFolakemi <olawumioderanti@gmail.com>
2023-02-27 16:10:34 -0800

    strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * swap_int - swaps the value of int a and int b
-*a: first int
-*b: second int
+*@a: first int
+*@b: second int
 * Return: 0
 */
 void swap_int(int *a, int *b)
@@ -11,5 +11,6 @@ void swap_int(int *a, int *b)
 
 	s = *a;
 	*a = *b;
-	*b = s;
+	*b = sn
+	return 0;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
  * Return: 0 is success
  * @s: to check
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int a = 0;
 
